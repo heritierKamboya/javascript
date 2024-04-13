d1b927d148c75f8cf188b17d7b88cf02e24e404c
Kamboya <kamboyah@gmail.com>
2024-04-12 18:15:35 -0700

    ajout du nouveau dossier dom.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import { fetchJSON } from "./functions/api.js";
+import { createElement } from "./functions/dom.js";
+try{
+    const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    console.log(todos)
+}catch(e){
+    const alertElement = createElement('div',{
+        class: 'alert alert-danger m-2',
+        role: 'alert'
+    })
+    alertElement.innerText='Impossible de charger les elements'
+    document.body.prepend(alertElement)
+}
