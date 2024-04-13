f9278e845d949f9d7effd16cc9ce3a9e19c80708
Kamboya <kamboyah@gmail.com>
2024-04-12 18:46:02 -0700

    ajout

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+import { TodoList } from "./components/TodoList.js";
 import { fetchJSON } from "./functions/api.js";
 import { createElement } from "./functions/dom.js";
 try{
     const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    console.log(todos)
+    const list = new TodoList(todos)
+    list.appendTo(document.querySelector('#todolist'))
 }catch(e){
     const alertElement = createElement('div',{
         class: 'alert alert-danger m-2',
